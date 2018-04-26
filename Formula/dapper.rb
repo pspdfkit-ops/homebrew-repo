3a3b4b0f82690eebeb226015672ebd98657cdc04
Roland Moriz <roland@moriz.de>
2018-04-26 23:39:34 +0200

    new release

--- Formula/dapper.rb
+++ Formula/dapper.rb
@@ -1,9 +1,9 @@
 class Dapper < Formula
   desc "PSPDFKit's maintained fork of Rancher's dapper"
   homepage "https://github.com/pspdfkit-ops/dapper"
-  url "https://github.com/pspdfkit-ops/dapper/releases/download/v0.3.4-PSPDFKit-0.0.1/dapper-Darwin-x86_64"
-  version "0.3.4-PSPDFKit-0.0.1"
-  sha256 "40b61146438ecfb8873e5728089e5c89cc3be0adf1b5aa7799dd622add92b04a"
+  url "https://github.com/pspdfkit-ops/dapper/releases/download/v0.3.4-PSPDFKit-1.0.0/dapper-Darwin-x86_64"
+  version "0.3.4-PSPDFKit-1.0.0"
+  sha256 "79300ae00d4a388f630410f841935894575d4452a66bbade9c8cc99887858f86"
 
   depends_on :arch => :x86_64
 
