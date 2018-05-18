b0b811ba4a2b890ea813f198305cee7e7286f8d7
Roland Moriz <roland@moriz.de>
2018-05-18 19:54:47 +0200

    Brew formula update for dapper version v0.3.4-PSPDFKit-1.3.1

--- Formula/dapper.rb
+++ Formula/dapper.rb
@@ -1,9 +1,9 @@
 class Dapper < Formula
   desc "PSPDFKit's maintained fork of Rancher's dapper"
   homepage "https://github.com/pspdfkit-ops/dapper"
-  url "https://github.com/pspdfkit-ops/dapper/releases/download/v0.3.4-PSPDFKit-1.3.0/dapper_0.3.4-PSPDFKit-1.3.0_darwin_amd64.tar.gz"
-  version "0.3.4-PSPDFKit-1.3.0"
-  sha256 "e36f63e4a5ede07a1c80a945fd229621f3806e5b177001a03b7c863f5ccc199f"
+  url "https://github.com/pspdfkit-ops/dapper/releases/download/v0.3.4-PSPDFKit-1.3.1/dapper_0.3.4-PSPDFKit-1.3.1_darwin_amd64.tar.gz"
+  version "0.3.4-PSPDFKit-1.3.1"
+  sha256 "9d917993763d18334f5a0ce4da409d6b4fc618a43099795c7e1e43de9b52b7fc"
 
   def install
     bin.install "dapper"
