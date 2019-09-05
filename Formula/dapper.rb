d4db4cef2a4e3d6a8c9554c761af2eb650ca9031
Roland Moriz <roland@moriz.de>
2019-09-05 13:21:57 +0200

    Brew formula update for dapper version v0.3.4-PSPDFKit-1.6.1

--- Formula/dapper.rb
+++ Formula/dapper.rb
@@ -2,16 +2,16 @@
 class Dapper < Formula
   desc "PSPDFKit's maintained fork of Rancher's dapper"
   homepage "https://github.com/pspdfkit-ops/dapper"
-  version "0.3.4-PSPDFKit-1.6.0"
+  version "0.3.4-PSPDFKit-1.6.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/pspdfkit-ops/dapper/releases/download/v0.3.4-PSPDFKit-1.6.0/dapper_0.3.4-PSPDFKit-1.6.0_darwin_amd64.tar.gz"
-    sha256 "a4b16be61484479354bc0906da8837ee38c9627f00288bda763f6b1aabcad2ec"
+    url "https://github.com/pspdfkit-ops/dapper/releases/download/v0.3.4-PSPDFKit-1.6.1/dapper_0.3.4-PSPDFKit-1.6.1_darwin_amd64.tar.gz"
+    sha256 "203f1da4edd9f763cd9093d4d7ce2f8e9069e04a0ddb43ad8c2a21d0428f82be"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/pspdfkit-ops/dapper/releases/download/v0.3.4-PSPDFKit-1.6.0/dapper_0.3.4-PSPDFKit-1.6.0_linux_amd64.tar.gz"
-      sha256 "cd0d9f63c4a9ec9a376f03970d3b0409e9e2b8a6f1dd9270a3a327fb8359d06e"
+      url "https://github.com/pspdfkit-ops/dapper/releases/download/v0.3.4-PSPDFKit-1.6.1/dapper_0.3.4-PSPDFKit-1.6.1_linux_amd64.tar.gz"
+      sha256 "dc6658dcb5c951982f027b4670c3730ba9af508b62bff3aeee4a5f74dc6872b0"
     end
   end
 
